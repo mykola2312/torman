850cc75e889d6feec4728d1f36dd37eb37634b45
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-06-17 00:29:41 +0300

    add clap. begin working on program

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
+use std::fs;
+use clap::{Parser, Subcommand};
+
 mod bencode;
 
+#[derive(Subcommand)]
+enum Command {
+    Index {
+        #[arg(short='p', long, help="path to transmission dir with torrents and resume")]
+        path: String
+    }
+}
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    #[command(subcommand)]
+    command: Command
+}
+
+fn index(path: &String) {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match &args.command {
+        Command::Index { path } => {
+            index(path);
+        }
+    }
 }
