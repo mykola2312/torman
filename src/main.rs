39c6078ea35238d4247e833a51f17278b548322c
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-06-17 19:46:05 +0300

    add sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::fs;
 use clap::{Parser, Subcommand};
+use rusqlite::Connection;
 
 mod bencode;
 
 #[derive(Subcommand)]
 enum Command {
     Index {
-        #[arg(short='p', long, help="path to transmission dir with torrents and resume")]
+        #[arg(short='p', long, default_value="torman.db", help="path to transmission dir with torrents and resume")]
         path: String
     }
 }
@@ -14,6 +15,9 @@ enum Command {
 #[derive(Parser)]
 #[command(version, about)]
 struct Args {
+    #[arg(short='d', long, help="path to sqlite database")]
+    db_path: String,
+
     #[command(subcommand)]
     command: Command
 }
@@ -24,6 +28,9 @@ fn index(path: &String) {
 
 fn main() {
     let args = Args::parse();
+
+    let db = Connection::open(args.db_path).unwrap();
+    db.execute("INSERT INTO `test` (test) VALUES (?1)", (1337,)).unwrap();
 
     match &args.command {
         Command::Index { path } => {
